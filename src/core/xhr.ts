0656a82cbccdbbee854332559ab59ad6a485b9a5
chang_su <chang_su@hunliji.com>
2020-11-21 16:14:23 +0800

    cookie

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,5 +1,7 @@
+import cookie from '../helpers/cookie'
 import { createError } from '../helpers/error'
 import { parseHeaders } from '../helpers/headers'
+import { isURLSameOrigin } from '../helpers/util'
 import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from '../types'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
@@ -12,7 +14,9 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       responseType,
       timeout,
       cancelToken,
-      withCredentials
+      withCredentials,
+      xsrfCookieName,
+      xsrfHeaderName
     } = config
 
     const request = new XMLHttpRequest()
@@ -34,6 +38,15 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     if (withCredentials) {
       request.withCredentials = true
     }
+    
+    if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName){
+      const xsrfValue = cookie.read(xsrfCookieName)
+      if (xsrfValue) {
+        headers[xsrfHeaderName!] = xsrfValue
+      }
+    }
+
+
 
     request.open(method.toUpperCase(), url!, true)
 
--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -41,3 +41,29 @@ export function deepMerge(...objs: any[]): any {
 
   return result
 }
+
+interface URLOrigin {
+  protocol: string
+  host: string
+}
+
+
+export function isURLSameOrigin(requestURL: string): boolean {
+  const parsedOrigin = resolveURL(requestURL)
+  return (
+    parsedOrigin.protocol === currentOrigin.protocol && parsedOrigin.host === currentOrigin.host
+  )
+}
+
+const urlParsingNode = document.createElement('a')
+const currentOrigin = resolveURL(window.location.href)
+
+function resolveURL(url: string): URLOrigin {
+  urlParsingNode.setAttribute('href', url)
+  const { protocol, host } = urlParsingNode
+
+  return {
+    protocol,
+    host
+  }
+}
